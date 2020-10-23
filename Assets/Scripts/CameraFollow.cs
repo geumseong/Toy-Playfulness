9dbb52a6e9e271981906e56a0100d955381343d1
Tobias <tobias.heukaeufer@ue-germany.de>
2020-10-23 17:02:38 +0200

    Implemented simple car controller; Added CameraFollow.cs; Added CarController.cs; Added 1x1-Pixel;

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float lerpSpeed;
+
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, lerpSpeed * Time.deltaTime);
+
+        Camera.main.orthographicSize -= Input.mouseScrollDelta.y;
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] float accelerationPower = 5f;
+    [SerializeField] float steeringPower = 5f;
+    float steeringAmount;
+    float moveSpeed;
+    float direction;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        steeringAmount = -Input.GetAxis("Horizontal");
+        moveSpeed = Input.GetAxis("Vertical") * accelerationPower;
+        direction = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up)));
+        rb.rotation += steeringAmount * steeringPower * rb.velocity.magnitude * direction;
+
+        rb.AddRelativeForce(Vector2.up * moveSpeed);
+
+        rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * steeringAmount / 2);
+    }
+}
