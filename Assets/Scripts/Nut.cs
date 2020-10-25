346be92162deb88b9871819536fb4ca67ada02ca
Tobias Heukäufer <tobias.heukaeufer@ue-germany.de>
2020-10-25 14:38:11 +0100

    Implemented CarGarage 1/2;

---
+++ Assets/Scripts/Nut.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nut : MonoBehaviour
+{
+    public Color tint;
+
+    private NutsManager nutsManager;
+    private SpriteRenderer spriteRenderer;
+    private Color standardColor;
+    private bool isRotating;
+
+    private void Start()
+    {
+        nutsManager = FindObjectOfType<NutsManager>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        standardColor = spriteRenderer.color;
+        isRotating = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isRotating)
+            StartCoroutine(RotateOut());
+    }
+
+    private void OnMouseOver()
+    {
+        if (!isRotating)
+            spriteRenderer.color = tint;
+    }
+
+    private void OnMouseExit()
+    {
+        if (!isRotating)
+            spriteRenderer.color = standardColor;
+    }
+
+    private IEnumerator RotateOut()
+    {
+        isRotating = true;
+        for (int i = 0; i < 144; i++)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + 5);
+            yield return null;
+        }
+
+
+        nutsManager.nuts.Remove(this);
+        nutsManager.NutCheck();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/NutsManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NutsManager : MonoBehaviour
+{
+
+    [HideInInspector] public List<Nut> nuts;
+
+    private void Start()
+    {
+        nuts = new List<Nut>();
+        foreach (Nut nut in GameObject.FindObjectsOfType<Nut>())
+        {
+            nuts.Add(nut);
+        }
+    }
+
+    public void NutCheck()
+    {
+        if (nuts.Count == 0)
+        {
+            Debug.Log("Initiate next sequence");
+            // Initiate next sequence
+        }
+    }
+}
