93daeb26d3f5af2fd3217d382c7a26d3f3394309
Tobias <tobias.heukaeufer@ue-germany.de>
2020-10-26 12:55:25 +0100

    Implemented MusicManager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private static MusicManager _instance;
+    public AudioClip total;
+    public AudioClip theme;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        if (_instance != null)
+        {
+            Debug.Log("Destroying duplicate MusicManager");
+            Destroy(gameObject);
+        }
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(ThemeCoroutine());
+    }
+
+    private IEnumerator ThemeCoroutine()
+    {
+        audioSource.clip = total;
+        audioSource.Play();
+        yield return new WaitForSeconds(total.length);
+        audioSource.clip = theme;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
