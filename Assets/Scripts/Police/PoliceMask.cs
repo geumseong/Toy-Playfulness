78c9e4455abdc614087413d61c85c37a7b112977
Tobias <tobias.heukaeufer@ue-germany.de>
2020-10-26 11:14:11 +0100

    Added sprites for Prisoners; Implemented random Prisoners and more juice for Police scene;

--- Assets/Scripts/Police/PoliceMask.cs
+++ Assets/Scripts/Police/PoliceMask.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PoliceMask : MonoBehaviour
 {
     public Vector3 offset;
+    public float scaleSpeed;
 
     private void Start()
     {
@@ -14,5 +15,19 @@ public class PoliceMask : MonoBehaviour
     private void Update()
     {
         transform.position = Vector3.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset, 7f * Time.deltaTime);
+    }
+
+    public void ScaleMaskUp()
+    {
+        StartCoroutine(WinDelay());
+    }
+
+    private IEnumerator WinDelay()
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(30, 30), scaleSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Police/PolicePrisoner.cs
+++ Assets/Scripts/Police/PolicePrisoner.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class PolicePrisoner : MonoBehaviour
 {
+    public Sprite[] sprites;
+    public SpriteRenderer[] exclamationMark;
+    private bool prisonerFound;
+
     private void Start()
     {
-        transform.position = new Vector2(Random.Range(-6.38f, 4.13f), Random.Range(6.38f, -4.13f));
+        GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Length)];
+        transform.position = new Vector2(Random.Range(-8f, 8f), Random.Range(3.4f, -3.4f));
+        gameObject.AddComponent<BoxCollider2D>();
+        exclamationMark[0].enabled = false;
+        exclamationMark[1].enabled = false;
     }
 
     private void OnMouseDown()
     {
+        if (!prisonerFound)
+            StartCoroutine(PrisonerFoundDelay());
+    }
+
+    private IEnumerator PrisonerFoundDelay()
+    {
+        prisonerFound = true;
+        FindObjectOfType<PoliceMask>().ScaleMaskUp();
+        exclamationMark[0].enabled = true;
+        exclamationMark[1].enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        GetComponent<Animator>().SetTrigger("PrisonerFound");
+        yield return new WaitForSeconds(0.5f);
         FindObjectOfType<MiniGameWon>().Win();
     }
 }
